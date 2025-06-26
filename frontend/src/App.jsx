eea5aa9dd68ec7c66c7fa634492baa054fd18744
sadafshahab12 <sadafshahab123@yahoo.com>
2025-06-25 22:36:07 -0700

    add scrolltotop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductList from "./pages/ProductList";
 import ProductDetails from "./pages/ProductDetails";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="font-jost">
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/product-details/:id" element={<ProductDetails />} />
         </Routes>
-          <Footer/>
+        <Footer />
       </div>
     </BrowserRouter>
   );
